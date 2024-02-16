ede9acdf5e578cdb577f5a5226d1610bc5328864
Someone-who-loves-coding <ps2284@srmist.edu.in>
2024-02-16 12:22:27 +0530

    Made a Model

--- SnapStock/SnapStock/settings.py
+++ SnapStock/SnapStock/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Homepage.apps.HomepageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
